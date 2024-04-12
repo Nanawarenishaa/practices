981313bc889249a6d7742ed96cde5b4e1928d636
Nanawarenishaa <164838299+Nanawarenishaa@users.noreply.github.com>
2024-04-12 20:34:00 +0530

    Add files via upload

---
+++ gcolor.js
@@ -0,0 +1,12 @@
+const btn=document.querySelectorAll(".btn");
+console.log(btn);
+const boxes=document.querySelectorAll(".box");
+console.log(boxes);
+const boxs=document.querySelector(".main__box");
+btn.forEach ((button) =>{
+    button.addEventListener("click", () =>{
+         let color=button.innerHTML;
+        
+       })
+
+    });
