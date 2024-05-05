50fa07ba90cace73fc4d6ff59ab74fc232961879
Nanawarenishaa <164838299+Nanawarenishaa@users.noreply.github.com>
2024-05-05 21:57:29 +0530

    Add files via upload

--- day13/index10.js
+++ day13/index10.js
@@ -37,36 +37,43 @@ const productsData = [
       }
     // Add more products as needed
   ];
-  
+  let sum=0;
   function renderProducts(products) {
     const productsContainer = document.getElementById('products');
     products.forEach(product => {
+      totalPrice.innerHTML=`<span>0.00</span>`;
       const card = document.createElement('div');
       card.classList.add('card');
       card.innerHTML = `
-        <img src="${product.image}" alt="${product.name}">
-        <h3>${product.name}</h3>
-        <p>${product.subtitle}</p>
-        <p>${product.price}</p>
-        <button onclick="addToSidebar('${product.name}', '${product.subtitle}', '${product.price}')">Add to List</button>
+      <img src="${product.image}" alt="${product.name}">
+      <h3>${product.name}</h3>
+      <p>${product.subtitle}</p>
+      <p>${product.price}</p>
+      <button onclick="addToSidebar('${product.name}', '${product.subtitle}', '${product.price}')">Add to List</button>
       `;
       productsContainer.appendChild(card);
+       
     });
+    
   }
   
   function addToSidebar(name, subtitle, price) {
     const listContainer = document.getElementById('list');
     const listItem = document.createElement('div');
-    listItem.innerHTML = `<span>${name}</span> <span> ${subtitle}</span> ${price}`;
+    listItem.innerHTML = `<span>${name}</span> <span> ${subtitle}</span> ${price} </span>`;
     listContainer.appendChild(listItem);
+    let numericPrice = Number(price.replace('$', ''));
+    sum += numericPrice;
+    const totalPrice = document.getElementById("totalPrice");
+    totalPrice.innerHTML = `<span>${sum.toFixed(2)}</span>`;
+  
   }
+  
   
   function toggleSidebar() {
     const sidebar = document.getElementById('sidebar');
     sidebar.classList.toggle('active');
   }
-    
   window.onload = () => {
     renderProducts(productsData);
-    updateTotalPrice();
   };
